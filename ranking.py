ebcc9040bfcf02105a383a1b8730c868ad1c1995
Hannes Haglund <hanha265@student.liu.se>
2018-12-02 10:17:10 +0100

    Ranking class instead of rank_score_pairs

---
+++ ranking.py
@@ -0,0 +1,50 @@
+
+class RankingEntry:
+    def __init__(self, player, score, rank):
+        self._player = player
+        self._score = score
+        self._rank = rank
+
+
+    def rank(self):
+        return self._rank
+
+
+    def player(self):
+        return self._player
+
+
+    def score(self):
+        return self._score
+
+
+
+class Ranking:
+    def __init__(self):
+        self._entries = []
+
+
+    def add_entry(self, player, score):
+        self._entries.append(RankingEntry(player, score, 0))
+        # Sort in proper order
+        self._entries = sorted(self._entries, key=lambda e: e.player().name())
+        self._entries = sorted(self._entries, key=lambda e: e.score())
+        self._entries.reverse() # Best to worst
+        for i,e in enumerate(self._entries):
+            e._rank = i+1
+
+
+    def entries(self):
+        return self._entries
+
+
+    def string(self):
+        rslt = ""
+        max_name_length = max([len(entry.player().name()) for entry in self._entries])
+        max_numerals = len(str(self._entries[-1].rank()))
+        for i,entry in enumerate(self._entries):
+            layout = "#{:<" + str(max_numerals) + "}: {: <" + str(max_name_length) + "} {}"
+            cols = [str(i), entry.player.name(), "("+str(entry.score())+" wins)"]
+            rslt += layout.format(*cols)
+            rslt += linesep
+        return rslt
--- tournament.py
+++ tournament.py
@@ -4,6 +4,7 @@ import itertools
 from os import linesep
 from player import Player
 from match_log import MatchLog
+from ranking import Ranking
 
 
 class Tournament:
@@ -45,29 +46,12 @@ class Tournament:
         self._match_log.add_bye(player)
 
 
-    def rank_score_pairs(self):
+    def ranking(self):
+        r = Ranking()
         active_players = [p for p in self._players if p.is_active()]
-        score_player_pairs = [(self._match_log.times_match_win(p), p) \
-                              for p in active_players]
-        pairs_by_name = sorted(score_player_pairs, \
-                               key=lambda e: e[1].name())
-        pairs_by_win = sorted(pairs_by_name, \
-                              key=lambda e: e[0])
-        pairs_by_win.reverse() # Best to worst
-        return pairs_by_win
-
-
-    def ranking_string(self):
-        rslt = ""
-        rank = self.rank_score_pairs()
-        max_name_length = max([len(e[1].name()) for e in rank])
-        max_numerals = len(str(len(rank)))
-        for i,e in enumerate(rank):
-            layout = "#{:<" + str(max_numerals) + "}: {: <" + str(max_name_length) + "} {}"
-            cols = [str(i), e[1].name(), "("+str(e[0])+" wins)"]
-            rslt += layout.format(*cols)
-            rslt += linesep
-        return rslt
+        for p in active_players:
+            r.add_entry(p, self._match_log.times_match_win(p))
+        return r
 
 
     def _player_name_player(self, player_name):
